7fc835771d85222552f82fb6c5e008af96d7ef78
Vinith Amma <vinithamma007@gmail.com>
2025-02-20 10:06:49 +0530

    tf practice

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-module "vpc" {
-  for_each = var.vpc
+module "components_vpc" {
 
   source    = "git::https://github.com/vinithrepo/tf-module-vpc.git"
 
+  for_each = var.vpc
 
   cidr = each.value["cidr"]
   subnets = each.value["subnets"]
--- vars.tf
+++ vars.tf
@@ -1,2 +1,2 @@
-
+variable "vpc" {}
 
