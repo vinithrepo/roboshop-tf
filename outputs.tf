3fa802eb263cc0443da074d47bf581b31ca80f6f
Vinith Amma <vinithamma007@gmail.com>
2025-07-10 00:37:15 +0530

    app module autoscalling group with all components

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
-output "vpc" {
-  value = module.vpc
+#output "vpc" {
+#  value = module.vpc
+#}
+#output "vpc" {
+#  value = data.aws_subnets.subnets.ids
+#}
+output "alb" {
+  value = module.alb
 }
